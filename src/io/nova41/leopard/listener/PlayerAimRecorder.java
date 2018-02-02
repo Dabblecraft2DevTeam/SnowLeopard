bfcc97e0a9f129d9df97ed6ea182f39ac19b731a
Unity41 <nascentnova@hotmail.com>
2018-02-02 13:47:32 +0800

    Removed AimData. abstract AimDataContainer -> SequenceContainer

--- src/io/nova41/leopard/listener/PlayerAimRecorder.java
+++ src/io/nova41/leopard/listener/PlayerAimRecorder.java
@@ -11,20 +11,26 @@ import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.util.Vector;
 
-import io.nova41.leopard.models.AimData;
 import io.nova41.leopard.models.AimDataContainer;
+import io.nova41.leopard.models.SequenceContainer;
 
 public class PlayerAimRecorder implements Listener {
-	private Map<String, AimData> listenedPlayer;
+	
+	/**
+	 * The job of this class is to calculate angles on every hit
+	 * and write them to the container.
+	 */
+	
+	private Map<String, SequenceContainer> listenedPlayer;
 
 	private JavaPlugin plugin;
-	private AimDataContainer dataContainer;
+	private SequenceContainer dataContainer;
 	
-	public PlayerAimRecorder(JavaPlugin plugin, AimDataContainer container) {
+	public PlayerAimRecorder(JavaPlugin plugin, SequenceContainer container) {
 		this.plugin = plugin;
 		this.dataContainer = container;
 		plugin.getServer().getPluginManager().registerEvents(this, plugin);
-		this.listenedPlayer = new HashMap<String, AimData>();
+		this.listenedPlayer = new HashMap<String, SequenceContainer>();
 	}
 
 	@EventHandler
@@ -55,12 +61,19 @@ public class PlayerAimRecorder implements Listener {
 			if (listenedPlayer.containsKey(playername))
 				return;
 			else
-				listenedPlayer.put(playername, new AimData());
+				listenedPlayer.put(playername, new SequenceContainer());
 		else
 			listenedPlayer.remove(playername);
 	}
+	
+	/**
+	 * Get the recorded aim data 
+	 * 
+	 * @param playername
+	 * @return
+	 */
 
-	public AimData getData(String playername) {
+	public AimDataContainer getData(String playername) {
 		return this.listenedPlayer.get(playername);
 	}
 
--- src/io/nova41/leopard/models/AimDataContainer.java
+++ src/io/nova41/leopard/models/AimDataContainer.java
@@ -1,32 +1,5 @@
 package io.nova41.leopard.models;
 
-import java.util.Iterator;
-import java.util.List;
+public class AimDataContainer {
 
-public abstract class AimDataContainer {
-
-	private List<Float> angleSequence;
-
-	public abstract Dataset toVector();
-
-	public void add(Float angle) {
-		this.angleSequence.add(angle);
-	}
-
-	protected double mean(List<Float> list) {
-		double sum = 0;
-		Iterator<Float> iter = list.iterator();
-		while (iter.hasNext())
-			sum += iter.next();
-		return sum / list.size();
-	}
-	
-	protected double dev(List<Float> list) {
-		double dev = 0;
-		double mean = mean(list);
-		Iterator<Float> iter = list.iterator();
-		while (iter.hasNext())
-			dev += Math.pow(iter.next() - mean, 2);
-		return dev;
-	}
 }
