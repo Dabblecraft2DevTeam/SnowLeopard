1e4583280fc42e5a0297692938963442641d87c8
Unity41 <nascentnova@hotmail.com>
2018-02-02 13:41:23 +0800

    Added some util methods to AimDataContainer

--- src/io/nova41/leopard/models/AimDataContainer.java
+++ src/io/nova41/leopard/models/AimDataContainer.java
@@ -1,6 +1,32 @@
 package io.nova41.leopard.models;
 
+import java.util.Iterator;
+import java.util.List;
+
 public abstract class AimDataContainer {
+
+	private List<Float> angleSequence;
+
 	public abstract Dataset toVector();
+
+	public void add(Float angle) {
+		this.angleSequence.add(angle);
+	}
+
+	protected double mean(List<Float> list) {
+		double sum = 0;
+		Iterator<Float> iter = list.iterator();
+		while (iter.hasNext())
+			sum += iter.next();
+		return sum / list.size();
+	}
 	
+	protected double dev(List<Float> list) {
+		double dev = 0;
+		double mean = mean(list);
+		Iterator<Float> iter = list.iterator();
+		while (iter.hasNext())
+			dev += Math.pow(iter.next() - mean, 2);
+		return dev;
+	}
 }
