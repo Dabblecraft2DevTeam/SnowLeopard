0bfa11cfe1c967e572b796d6c6d13208cb064fa5
Nova41 <nascentnova@hotmail.com>
2018-02-03 13:07:00 +0800

    CommandMap -> CommandManager

--- src/io/nova41/leopard/commands/CommandManager.java
+++ src/io/nova41/leopard/commands/CommandManager.java
@@ -6,52 +6,60 @@ import java.util.Map;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public class CommandMap implements CommandExecutor {
-	
-	private LeopardCommand defaultCommand;
+public class CommandManager implements CommandExecutor {
+
 	private LeopardCommand errorCommand;
+	private LeopardCommand usageCommand;
 	private Map<String, LeopardCommand> commands;
-	
+
 	/**
-	 * Specifiy the base command of the commandmap.
-	 * e.g. The base command of '/game stop' is 'game'
+	 * Specifiy the base command of the commandmap. e.g. The base command of
+	 * '/game stop' is 'game'
 	 * 
 	 * @param baseCommand
 	 */
-	public CommandMap(JavaPlugin plugin, String baseCommand) {
+	public CommandManager(JavaPlugin plugin, String baseCommand) {
 		plugin.getCommand(baseCommand).setExecutor(this);
 		this.commands = new HashMap<String, LeopardCommand>();
 	}
-	
+
 	/**
 	 * The command will be executed when there are no arguments
+	 * 
 	 * @param executor
 	 */
-	
+
 	public void setDefaultCommand(LeopardCommand executor) {
-		defaultCommand = executor;
+		usageCommand = executor;
 	}
-	
+
 	public void setErrorCommand(LeopardCommand executor) {
 		errorCommand = executor;
 	}
-	
+
 	public void registerCommand(String name, LeopardCommand executor) {
 		this.commands.put(name.toLowerCase(), executor);
 	}
-	
+
 	@Override
 	public boolean onCommand(CommandSender sender, Command cmd, String str, String[] args) {
-		if (args.length == 0 && this.defaultCommand != null)
-			this.defaultCommand.perform(sender, args);
-		else if (this.commands.containsKey(cmd.getName().toLowerCase())) {
-			this.commands.get(cmd.getName().toLowerCase()).perform(sender, args);
+		if (args.length == 0 && usageCommand != null)
+			this.usageCommand.perform(sender, args);
+		else if (commands.containsKey(cmd.getName().toLowerCase())) {
+			LeopardCommand executor = commands.get(cmd.getName().toLowerCase());
+			if (sender instanceof Player)
+				executor.perform(sender, args);
+			else if (errorCommand != null)
+				errorCommand.perform(sender, args);
+			else 
+				return false;
 			return true;
 		}
-		if (this.errorCommand != null) {
-			this.errorCommand.perform(sender, args);
+		if (usageCommand != null) {
+			usageCommand.perform(sender, args);
 			return true;
 		}
 		return false;
---
+++ src/io/nova41/leopard/commands/CommandVersion.java
@@ -0,0 +1,18 @@
+package io.nova41.leopard.commands;
+
+import org.bukkit.command.CommandSender;
+
+public class CommandVersion implements LeopardCommand {
+
+	@Override
+	public boolean isPlayerOnly() {
+		return true;
+	};
+
+	@Override
+	public void perform(CommandSender sender, String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
