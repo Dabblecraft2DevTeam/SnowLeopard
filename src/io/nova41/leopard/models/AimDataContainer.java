ca3f84204eda8220a18dac484680f31c6bcd5aeb
Nova41 <nascentnova@hotmail.com>
2018-02-11 11:32:39 +0800

    Fixed: error calculation

--- src/io/nova41/leopard/models/AimDataContainer.java
+++ src/io/nova41/leopard/models/AimDataContainer.java
@@ -1,6 +1,7 @@
 package io.nova41.leopard.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import org.bukkit.entity.Entity;
@@ -9,32 +10,41 @@ import org.bukkit.util.Vector;
 
 public class AimDataContainer extends SequenceContainer {
 
+	protected List<Float> deltaSequence;
+
+	public AimDataContainer() {
+		super();
+		this.deltaSequence = new ArrayList<Float>();
+	}
+
 	@Override
 	public Dataset toVector(String category) {
-		// vector to be returned
-		Double[] vector = new Double[4];
+		if (this.angleSequence.size() == 1)
+			return new Dataset(category, this.emptyVector);
 
-		// the delta of the angle sequence
-		List<Float> delta_of_angles = new ArrayList<Float>();
-		for (int i = 1; i >= angleSequence.size(); i++)
-			delta_of_angles.add(angleSequence.get(i) - angleSequence.get(i - 1));
+		// the vector to be returned
+		Double[] vector = new Double[4];
 
 		// construct the vector double array
 		vector[0] = dev(angleSequence);
-		vector[1] = dev(delta_of_angles);
+		vector[1] = dev(deltaSequence);
 		vector[2] = mean(angleSequence);
-		vector[3] = mean(delta_of_angles);
+		vector[3] = mean(deltaSequence);
 		return new Dataset(category, vector);
 	}
 
 	@Override
-	public void input(Player player, Entity enemy) {
+	public void input(Player player, Entity enemy, boolean verboseOutput) {
 		Vector playerLookDir = player.getEyeLocation().getDirection();
 		Vector playerEyeLoc = player.getEyeLocation().toVector();
 		Vector entityLoc = enemy.getLocation().toVector();
 		Vector playerEntityVec = entityLoc.subtract(playerEyeLoc);
 		float angle = playerLookDir.angle(playerEntityVec);
+		if (this.angleSequence.size() > 1) {
+			this.deltaSequence.add(Math.abs(angle - angleSequence.get(angleSequence.size() - 1)));
+		}
 		this.angleSequence.add(angle);
+		System.out.println("Current hit angle: " + angle);
 	}
 
 }
